b882b4118f9eeb5c44b773e22213dfaf3e184c8c
1v4nP4 <1v4nP4@proton.me>
2025-08-06 18:46:40 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //Generador de excusas:Iván Padrón(A.K.A Spidy)
+  //Empezaré haciendo los arrays de excusas
+ 
+
+  //funcion de excuse generator
+  const generadorExcusas =(excusa) =>{
+    //array con los componentes de la excusa 
+    const who = ["La abuela" , "El perro" , "Mi novia" , "El gato"];
+    const action = ["se comió" , "rompió" , "mojó"  , "bugeó"];
+    const what  = ["la tarea" , "el portatil" , "el codespace" , "el cable"];
+    const where = ["en mi casa" , "en la calle" , "en el suelo" , "en github"];
+    const when = ["ayer." , "el otro día." , "en verano." , "la semana pasada."];
+    //función que escoje un componente de cada array aleatoriamente
+    const randomizadorDeExcusa = array => array[Math.floor(Math.random()*array.length)]
+
+   
+    const excusaAleatoria = `${randomizadorDeExcusa(who)} ${randomizadorDeExcusa(action)} ${randomizadorDeExcusa(what)} ${randomizadorDeExcusa(where)} ${randomizadorDeExcusa(when)}..`;
+    return excusaAleatoria;
+   
+  }
+ console.log(generadorExcusas());
+ const parrafo = document.getElementById("Excusa");
+ parrafo.innerHTML = generadorExcusas();
+
+
+
+
+  
+}
